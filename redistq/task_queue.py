3e4936416ee7a3f96be0f98c8770203792440861
Bastian Venthur <bastian.venthur@flixbus.com>
2021-04-09 10:51:32 +0200

    initial import

---
+++ redistq/task_queue.py
@@ -0,0 +1,346 @@
+import json
+import logging
+import time
+from uuid import uuid4
+
+import redis
+
+import yield_curve_forecast.config as config
+
+logger = logging.getLogger(__name__)
+
+
+class TaskQueue:
+    """Reliable task queue.
+
+    This task queue is able to recover failed task, see `TTL` in `add`.
+
+    On the producing side:
+
+    >>> tq = TaskQueue('localhost', 'myqueue')
+    >>> for i in range(10):
+    ...     tq.add(some task)
+
+    On the consuming side:
+
+    >>> tq = TaskQueue('localhost', 'myqueue')
+    >>> while True:
+    ...     task, task_id = tq.get(lease_timeout)
+    ...     if task is not None:
+    ...         # do something
+    ...         tq.complete(task_id)
+    ...     if tq.is_empty():
+    ...         break
+
+    """
+
+    def __init__(self, host, name, reset=False):
+        """Initialize the task queue.
+
+        Note: a task has to be at any given time either in the task
+        queue or in the processing queue. If a task is moved from one
+        queue to the other it has to be in an atomic fashion!
+
+        Parameters
+        ----------
+        host : str
+            hostname of the redis server
+        name : str
+            name of the task queue
+        reset : bool
+            If true, reset existing keys in the DB that have `name` as
+            prefix.
+
+        """
+        self._queue = name + ':queue'
+        self._processing_queue = name + ':processing'
+        self._tasks = name + ':tasks:'
+        self._host = host
+
+        # timeout for getting new tasks + waiting for producer to
+        # generate ones
+        self.timeout = config.WORKER_TIMEOUT * 60  # minutes
+        self.socket_timeout = 25
+
+        self.connect()
+
+        if reset:
+            self._reset()
+
+    def connect(self):
+        """Establish a connection to Redis.
+        If a connection already exists, it's overwritten.
+        """
+        # We use health_check_interval to avoid:
+        # https://github.com/andymccurdy/redis-py/issues/1186
+        self.conn = redis.Redis(
+            host=self._host,
+            health_check_interval=30,
+            socket_timeout=self.socket_timeout,
+        )
+
+    def __len__(self):
+        return (self.conn.llen(self._queue)
+                + self.conn.llen(self._processing_queue))
+
+    def add(self, task, ttl=3):
+        """Add a task to the task queue.
+
+        Parameters
+        ----------
+        task : something that can be JSON-serialized
+        ttl : int
+            Number of (re-)tries, including the initial one, in case the
+            job dies.
+
+        """
+        # we wrap the task itself with some meta data
+        id_ = uuid4().hex
+        ttl = ttl
+        wrapped_task = {
+            'ttl': ttl,
+            'task': task,
+            'deadline': None,
+        }
+        task = self._serialize(wrapped_task)
+        # store the task + metadata
+        self.conn.set(self._tasks + id_, task)
+        # put task-id into the task queue
+        self.conn.lpush(self._queue, id_)
+
+    def get(self, lease_timeout):
+        """Get a task from the task queue.
+
+        This method returns the next task from the queue. It also puts
+        this task into an internal processing queue so we can keep track
+        on ongoing tasks. In order to mark that task as done, you have
+        to use:
+
+            >>> task, task_id = taskqueue.get()
+            >>> # do something
+            >>> taskqueue.complete(task_id)
+
+        After some time (i.e. `lease_timeout`) tasks expire and are
+        removed from the processing queue and the TTL is decreased by
+        one. If TTL is still > 0 the task will be put back into the task
+        queue for retry.
+
+        Parameters
+        ----------
+        lease_timeout : int
+            lease timeout in seconds, i.e. how much time we give the
+            task to process until we assume it died
+
+        Returns
+        -------
+        (task, task_id) :
+            The next item from the task list or (None, None) if it's
+            empty
+
+        """
+        # See comment in __init__ about moving jobs between queues in an
+        # atomic fashion before you modify!
+        # we need to deal with the socket timeout,
+        # see https://github.com/andymccurdy/redis-py/issues/1305
+        for _ in range(1 + self.timeout // (self.socket_timeout - 5)):
+            try:
+                task_id = self.conn.brpoplpush(
+                    self._queue,
+                    self._processing_queue,
+                    self.socket_timeout - 5,
+                    )
+            except redis.exceptions.TimeoutError:
+                logger.exception('Redis timeout, will reset the connection')
+                self.connect()
+                # we consider the task as None in case of timeout
+                # to handle it like an operational timneout
+                task_id = None
+            if task_id is None:
+                continue
+            else:
+                break
+
+        if task_id is None:
+            return None, None
+        task_id = task_id.decode()
+        logger.info(f'Got task with id {task_id}')
+
+        now = time.time()
+        deadline = now + lease_timeout
+        task = self._deserialize(self.conn.get(self._tasks + task_id))
+        task['deadline'] = deadline
+        self.conn.set(self._tasks + task_id, self._serialize(task))
+
+        return task['task'], task_id
+
+    def complete(self, task_id):
+        """Mark a task as completed.
+
+        Marks a task as completed and removes it from the processing
+        queue.
+
+        If the job is in the queue, which happens if it took too long
+        and it expired, is removed from that too.
+
+
+        Parameters
+        ----------
+        task_id : str
+            the task ID
+
+        """
+        logger.info(f'Marking task {task_id} as completed')
+        removed = self.conn.lrem(self._processing_queue, 0, task_id)
+        # check if we actually removed something
+        if removed == 0:
+            logger.warning(f'Task {task_id} was not being processed, '
+                           'so cannot mark as completed.')
+            # this happens because another worker marked it as expired
+            # so it was put back into the queue
+            # now we remove it to avoid a futile re-processing
+            deleted_retry = self.conn.lrem(self._queue, 0, task_id)
+            # if it's not in the queue, this was the last attempt
+            # allowed by the TTL
+            if deleted_retry == 0:
+                logger.warning(f'Task {task_id} was not not in the queue,'
+                               ' this was the last attempt and succeeded.')
+        # delete the lease as well
+        self.conn.delete(self._tasks + task_id)
+
+    def reschedule(self, task_id):
+        """Move a task back from the processing- to the task queue.
+
+        Workers can use this method to "drop" a work unit in case of
+        eviction.
+
+        This function does not modify the TTL.
+
+        Parameters
+        ----------
+        task_id : str
+            the task ID
+
+        Raises
+        ------
+        ValueError :
+            Task is not being processed, and cannot be re-scheduled
+
+        """
+        # See comment in __init__ about moving jobs between queues in an
+        # atomic fashion before you modify!
+        with self.conn.pipeline() as pipeline:
+            while True:
+                try:
+                    pipeline.watch(self._processing_queue, self._tasks)
+                    procs = pipeline.lrange(self._processing_queue, 0, -1)
+                    procs = [p.decode() for p in procs]
+                    if task_id not in procs:
+                        raise ValueError(f'Task {task_id} does not exist.')
+                    task = self.conn.get(self._tasks + task_id)
+
+                    pipeline.multi()
+                    pipeline.lrem(self._processing_queue, 0, task_id)
+                    pipeline.lpush(self._queue, task_id)
+
+                    task = self._deserialize(task)
+                    task['deadline'] = None
+                    task = self._serialize(task)
+                    self.conn.set(self._tasks + task_id, task)
+
+                    pipeline.execute()
+                    break
+                except redis.WatchError:
+                    # processing queue has been modified since we tried to
+                    # execute the atomic block, the task might not be in
+                    # there anymore, retry
+                    continue
+
+    def is_empty(self):
+        """Check if the task queue is empty (todo and processing).
+
+        This method relies on a job being either in the task- or in the
+        processing queue at any given time, see comment in __init__.
+
+        Internally, this function also checks the currently processed
+        tasks for expiration and teals with TTL and re-scheduling them
+        into the task queue.
+
+        Returns
+        -------
+        bool
+
+        """
+        self._check_expired_leases()
+        return (self.conn.llen(self._queue) == 0
+                and self.conn.llen(self._processing_queue) == 0)
+
+    def _check_expired_leases(self):
+        """Check for expired leases.
+
+        This method goes through all tasks that are currently processed
+        and checks if their deadline expired. If not we assume the
+        worker died. We decrease the TTL and if TTL is still > 0 we
+        reschedule the task into the task queue or, if the TTL is
+        exhausted, we remove the task completely.
+
+        """
+        # to through all tasks that we currently have and check the ones
+        # with expired timeout
+        now = time.time()
+        for key in self.conn.scan_iter(match=self._tasks + '*'):
+
+            key = key.decode()
+            id_ = key.split(':')[-1]
+
+            task = self.conn.get(key)
+            task = self._deserialize(task)
+
+            if task['deadline'] is None:
+                # task hasn't started yet, we don't care
+                continue
+
+            if task['deadline'] <= now:
+                logger.warning(
+                    f'A lease expired, probably job failed: {key} => {task}')
+                task['ttl'] -= 1
+                if task['ttl'] <= 0:
+                    logger.error(f'Job {task} with it {id_ }failed too many'
+                                 ' times, dropping it.')
+                    self.conn.lrem(self._processing_queue, 0, id_)
+                    self.conn.delete(key)
+                    continue
+
+                task['deadline'] = None
+                task = self._serialize(task)
+                # See comment in __init__ about moving jobs between
+                # queues in an atomic fashion before you modify!
+                with self.conn.pipeline() as pipeline:
+                    while True:
+                        try:
+                            pipeline.watch(key, self._processing_queue)
+                            pipeline.multi()
+                            pipeline.set(key, task)
+                            pipeline.lrem(self._processing_queue, 0, id_)
+                            pipeline.lpush(self._queue, id_)
+                            pipeline.execute()
+                            break
+                        except redis.WatchError:
+                            continue
+
+    def _serialize(self, task):
+        task = json.dumps(task, sort_keys=True)
+        return task
+
+    def _deserialize(self, blob):
+        task = json.loads(blob)
+        return task
+
+    def _reset(self):
+        """Delete all keys in the DB prefixed with our name.
+
+        """
+        self.conn.delete(self._queue)
+        self.conn.delete(self._processing_queue)
+        keys = self.conn.keys(f'{self._tasks}*')
+        if keys:
+            self.conn.delete(*keys)
---
+++ tests/test_task_queue.py
@@ -0,0 +1,266 @@
+import logging
+import time
+import uuid
+
+import pytest
+import redis
+
+from yield_curve_forecast import task_queue
+
+
+REDIS_HOST = 'localhost'
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def taskqueue():
+
+    name = str(uuid.uuid4())
+
+    tq = task_queue.TaskQueue(REDIS_HOST, name)
+
+    yield tq
+
+    # delete the stuff
+    tq._reset()
+
+
+def _has_redis():
+    try:
+        rs = redis.Redis(host=REDIS_HOST)
+        rs.ping()
+    except Exception:
+        return False
+    return True
+
+
+# decorator to check if we're running with redis
+has_redis = pytest.mark.skipif(
+    not _has_redis(),
+    reason='requires Redis running on {}'.format(REDIS_HOST)
+)
+
+
+@has_redis
+def test_add(taskqueue):
+    # add two tasks and get them back in correct order
+    TASKS = ['foo', 'bar']
+    for task in TASKS:
+        taskqueue.add(task)
+
+    task, _ = taskqueue.get(10)
+    assert task == TASKS[0]
+    task, _ = taskqueue.get(10)
+    assert task == TASKS[1]
+
+
+@has_redis
+def test_get(taskqueue):
+    taskqueue.timeout = 1
+    TASK = 'foo'
+    taskqueue.add(TASK)
+    task, _ = taskqueue.get(10)
+    assert task == TASK
+    # calling on empty queue returns None
+    assert taskqueue.get(10) == (None, None)
+
+
+@has_redis
+def test_complete(taskqueue):
+    TIMEOUT = 1
+
+    # boring case
+    taskqueue.add('foo', ttl=1)
+    _, id_ = taskqueue.get(TIMEOUT)
+    assert not taskqueue.is_empty()
+    taskqueue.complete(id_)
+    assert taskqueue.is_empty()
+
+    # interesting case: we complete the task after it expired already
+    taskqueue.add('foo', ttl=1)
+    _, id_ = taskqueue.get(TIMEOUT)
+    time.sleep(TIMEOUT + 1)
+    assert taskqueue.is_empty()
+    taskqueue.complete(id_)
+    assert taskqueue.is_empty()
+
+
+@has_redis
+def test_complete_warning(taskqueue, caplog):
+    taskqueue.add('foo')
+    _, id_ = taskqueue.get(1)
+    caplog.clear()
+    taskqueue.complete(id_)
+    assert "was not being processed" not in caplog.text
+    caplog.clear()
+    taskqueue.complete(id_)
+    assert "was not being processed" in caplog.text
+
+
+@has_redis
+def test_is_empty(taskqueue):
+    assert taskqueue.is_empty()
+
+    taskqueue.add('foo')
+    assert not taskqueue.is_empty()
+
+    task, id_ = taskqueue.get(10)
+    assert not taskqueue.is_empty()
+
+    taskqueue.complete(id_)
+    assert taskqueue.is_empty()
+
+
+@has_redis
+def test_expired(taskqueue):
+    TIMEOUT = 1
+    taskqueue.timeout = TIMEOUT
+    taskqueue.add('foo', 1)
+    taskqueue.get(TIMEOUT)
+    assert not taskqueue.is_empty()
+    time.sleep(TIMEOUT + 1)
+    assert taskqueue.is_empty()
+
+    for i in range(5):
+        taskqueue.add(i)
+
+    tstart = time.time()
+    while not taskqueue.is_empty():
+        taskqueue.get(TIMEOUT)
+    tend = time.time()
+    assert tend - tstart > TIMEOUT
+
+
+@has_redis
+def test_ttl(taskqueue, caplog):
+    TIMEOUT = 1
+    taskqueue.timeout = TIMEOUT
+    taskqueue.add('foo', ttl=3)
+
+    # start a task and let it expire...
+    taskqueue.get(TIMEOUT)
+    time.sleep(TIMEOUT + 1)
+    # check and put it back into task queue
+    assert not taskqueue.is_empty()
+
+    # second attempt...
+    taskqueue.get(TIMEOUT)
+    time.sleep(TIMEOUT + 1)
+    assert not taskqueue.is_empty()
+
+    # third attempt... *boom*
+    taskqueue.get(TIMEOUT)
+    time.sleep(TIMEOUT + 1)
+    caplog.clear()
+    assert taskqueue.is_empty()
+    assert "failed too many times" in caplog.text
+
+
+@has_redis
+def test_reschedule(taskqueue):
+    taskqueue.timeout = 1
+    taskqueue.add('foo')
+    _, id_ = taskqueue.get(10)
+    # task queue should be empty as 'foo' is in the processing queue
+    assert taskqueue.get(10) == (None, None)
+
+    taskqueue.reschedule(id_)
+    task, _ = taskqueue.get(10)
+    assert task == 'foo'
+
+
+@has_redis
+def test_reschedule_error(taskqueue):
+    with pytest.raises(ValueError):
+        taskqueue.reschedule('bar')
+
+
+@has_redis
+def test_full(taskqueue):
+    TASKS = ['FOO', 'BAR', 'BAZ']
+    for t in TASKS:
+        taskqueue.add(t)
+
+    counter = 0
+    while True:
+        task, task_id = taskqueue.get(1)
+        if task is not None:
+            taskqueue.complete(task_id)
+            counter += 1
+        if taskqueue.is_empty():
+            break
+
+    assert counter == len(TASKS)
+
+
+@has_redis
+def test_complete_rescheduled_task(taskqueue):
+    TIMEOUT = 1
+    TASK_CONTENT = 'sloth'
+    taskqueue.timeout = TIMEOUT
+    taskqueue.add(TASK_CONTENT, ttl=3)
+
+    # start a task and let it expire...
+    _, task_id = taskqueue.get(TIMEOUT)
+    time.sleep(TIMEOUT + 1)
+
+    # check and put it back into task queue
+    assert not taskqueue.is_empty()
+
+    # now the task is completed, although it took a long time...
+    taskqueue.complete(task_id)
+
+    # it is NOT in the taskqueue, because it was finished
+    assert taskqueue.is_empty()
+
+
+@has_redis
+def test_tolerate_double_completion(taskqueue):
+    TIMEOUT = 1
+    TASK_CONTENT = 'sloth'
+    taskqueue.timeout = TIMEOUT
+    taskqueue.add(TASK_CONTENT, ttl=3)
+
+    # start a task and let it expire...
+    task, task_id = taskqueue.get(TIMEOUT)
+    time.sleep(TIMEOUT + 1)
+
+    # check and put it back into task queue
+    assert not taskqueue.is_empty()
+
+    # get it again
+    _, task_redo_id = taskqueue.get(TIMEOUT)
+    assert task_redo_id == task_id
+
+    # now the task is completed, although it took a long time...
+    taskqueue.complete(task_id)
+
+    # but the other worker doesn't know and keep processing, until...
+    taskqueue.complete(task_redo_id)
+
+    # no crashes, the double completion is fine and queues are empty
+    assert taskqueue.is_empty()
+
+
+@has_redis
+def test_task_queue_len(taskqueue):
+
+    # empty queue
+    assert len(taskqueue) == 0
+
+    # insert two tasks
+    TASKS = ['foo', 'bar']
+    for task in TASKS:
+        taskqueue.add(task)
+    assert len(taskqueue) == len(TASKS)
+
+    # removing getting the tasks w/o completing them
+    ids = []
+    for task in TASKS:
+        ids.append(taskqueue.get(10)[1])
+    assert len(taskqueue) == len(TASKS)
+
+    for id_ in ids:
+        taskqueue.complete(id_)
+    assert len(taskqueue) == 0
