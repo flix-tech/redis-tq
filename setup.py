59f4710cd33043c938bc013085d3f1610f1925ba
Bastian Venthur <bastian.venthur@flixbus.com>
2021-04-09 11:24:12 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+meta = {}
+exec(open('./redistq/version.py').read(), meta)
+meta['long_description'] = open('./README.md').read()
+
+setup(
+    name='redis-tq',
+    version=meta['__version__'],
+    description='Redis Based Task Queue',
+    long_description=meta['long_description'],
+    long_description_content_type='text/markdown',
+    keywords='redis task queue',
+    author="Team Bus Factor",
+    author_email="team.bus-factor@flixbus.com",
+    # FIXME!
+    url='https://example.com',
+    python_requires='>=3.8',
+    install_requires=[
+        'redis',
+    ],
+    packages=['redistq'],
+    license='MIT',
+)
